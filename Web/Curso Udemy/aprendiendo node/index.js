17d0222941a02db234223bd04d93aab04ce97033
sachondo7 <80768647+sachondo7@users.noreply.github.com>
2023-07-09 13:24:29 -0400

    s

--- Web/Curso Udemy/aprendiendo node/index.js
+++
@@ -1,5 +0,0 @@
-const fs = require ("fs"); 
-var superheroes = require("superheroes");
-// fs.copyFileSync("source, destination");
-var mysuperhero = superheroes.random();
-console.log(mysuperhero);
